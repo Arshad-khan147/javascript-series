01f24328ef968c9db0a40371fbc1da49445f62e6
ARSHAD KHAN <75555991+Arshad-khan147@users.noreply.github.com>
2023-07-10 17:30:27 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,27 @@
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+const arr=[2,3,4,5,6];
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// concatenate one array from another arr
+
+// const arr1 = [3,4,5];
+// const arr2 = [...arr,...arr1];
+// console.log(arr2.length);
+
+// console.log(arr.slice(1,3));
+// console.log(arr);
+// console.log(arr.splice(1,3));
+// console.log(arr);
+// console.log(Array.isArray(arr));
+// console.log(Array.from(arr));
+// console.log(Array.from(arr));
+let val1 = 200;
+let val2 = 300;
+let val3 = 400;
+// console.log(Array.of(val1,val2,val3));
+console.log(arr.reverse());
